f93e41dc45b55b0b9f13690fe462c2010dca7f08
rachidesper <rachidesper290@gmail.com>
2025-10-10 10:08:37 -0300

    Creada interfaz de pedido service

--- src/main/java/service/interfaces/PedidoService.java
+++ src/main/java/service/interfaces/PedidoService.java
@@ -3,7 +3,15 @@ package service.interfaces;
 public class PedidoService {
 
 	public PedidoService() {
-		// TODO Auto-generated constructor stub
+		// definir los metodos que van a tener cada implementacion, 
+		//y despues en la implementacion hago la logica de negocio
+		// engancha lo que llega con lo que tiene que salir y en el medio se mete logica adicional, modificaciones,validaciones, etc
 	}
-
+		/*
+		 * ej finalizarpedido(){
+		 * validar que los productos esten en stock 
+		 * ver si hay productos en el pedido
+		 * 
+		 * }
+		 */
 }
