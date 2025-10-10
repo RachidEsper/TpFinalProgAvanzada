d836f6bd83a6b143dc3212f9c686c8c8923c72e6
rachidesper <rachidesper290@gmail.com>
2025-10-10 10:02:49 -0300

    Creadas carpetas mapper para DTO y DAO

---
+++ src/main/java/dao/mapper/ProductoMapperDAO.java
@@ -0,0 +1,13 @@
+package dao.mapper;
+
+public class ProductoMapperDAO {
+
+	public ProductoMapperDAO() {
+		/*
+		 * escribo los metodos para convertir los datos crudos que vienen de la DB
+		 * en objeto o clase java, en caso de un findbyidproducto lo convierto a un 
+		 * producto de tipo Producto
+		 */
+	}
+
+}
