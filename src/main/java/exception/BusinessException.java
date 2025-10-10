6c67ebb4c40656c6ef5daaaa7bc0b96b36c6e3ad
rachidesper <rachidesper290@gmail.com>
2025-10-10 10:07:58 -0300

    Creado excepciones de negocio

--- src/main/java/exception/BusinessException.java
+++ src/main/java/exception/BusinessException.java
@@ -3,7 +3,11 @@ package exception;
 public class BusinessException {
 
 	public BusinessException() {
-		// TODO Auto-generated constructor stub
+		/*
+		 * son los mensajes de error que le voy a pasar al front
+		 * el formato de una excepcion es el n y mensaje 
+		 * la idea es hacer excepciones personalizadas
+		 */
 	}
 
 }
