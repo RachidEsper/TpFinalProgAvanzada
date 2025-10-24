2870e6c3836e1ddb15fb1cfef57f799cf256e1be
rachidesper <rachidesper290@gmail.com>
2025-10-24 01:52:14 -0300

    archivo de pruebas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
+import model.Categoria;
 import config.DbConfig;
+import dao.implementation.CategoriaDAOImpl;
 
 public class Main {
 	public static void main(String[] args) {
 		// 4.1) Usamos el Singleton y probamos un SELECT 1 (smoke test)
-		try (Connection cn = DbConfig.getInstance().getConnection();
+/*		try (Connection cn = DbConfig.getInstance().getConnection();
 				PreparedStatement ps = cn.prepareStatement("SELECT 1");
 				ResultSet rs = ps.executeQuery()) {
 
@@ -18,7 +19,23 @@ public class Main {
 			// 4.2) Mostrar el error completo para diagnosticar en esta etapa
 			e.printStackTrace();
 		}
-
+		*/
+		CategoriaDAOImpl categoriaDAO = new CategoriaDAOImpl();
+		
+		
+		
+		//System.out.println("ID: "+categoriaDAO.findbyid(5).getIdCategoria()+
+			//	" Nombre: "+categoriaDAO.findbyid(5).getNombre()
+				//+" Descripcion: "+categoriaDAO.findbyid(5).getDescripcion());
+		
+		categoriaDAO.deleteById(6);
+		
+		
+		System.out.println("Probando metodo findById:");
+		System.out.println("ID: "+categoriaDAO.findbyid(1).getIdCategoria()+
+				" Nombre: "+categoriaDAO.findbyid(1).getNombre()
+				+" Descripcion: "+categoriaDAO.findbyid(1).getDescripcion());
+		
 		/*
 		 * public static void main(String[] args) { // aca hago pruebas de test
 		 * unitarios try (Connection conn = Db.config.getInstance().getConnection();
