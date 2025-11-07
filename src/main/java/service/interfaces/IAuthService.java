0e72e505489516860b2433b57496dd17f9ba1846
rachidesper <rachidesper290@gmail.com>
2025-11-07 01:52:49 -0300

    creada interfaz user service

---
+++ src/main/java/service/interfaces/IAuthService.java
@@ -0,0 +1,24 @@
+package service.interfaces;
+
+import model.Usuario;
+
+public interface IAuthService {
+
+	// buscar si usuario existe
+	// ver si la contraseña es correcta
+	// devolver el usuario si todo ok
+	// devolver null si no ok
+	
+	Usuario login(String email, String contrasenia);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
