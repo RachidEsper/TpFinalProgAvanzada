b9442b6f2f4678b97fa0a0f35a7f1a90c2f234bd
rachidesper <rachidesper290@gmail.com>
2025-11-26 02:40:22 -0300

    Servlets creados usuario normal

---
+++ src/main/java/web/usuario/ListadoUsuarios.java
@@ -0,0 +1,50 @@
+package web.usuario;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Usuario;
+
+import java.io.IOException;
+import java.util.List;
+
+import dao.implementation.UsuarioDAOImpl;
+import dao.interfaces.IUsuarioDAO;
+
+/**
+ * Servlet implementation class ListadoUsuarios
+ */
+@WebServlet("/ListadoUsuarios")
+public class ListadoUsuarios extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListadoUsuarios() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		IUsuarioDAO usuarioDAO = new UsuarioDAOImpl();
+		List<Usuario> usuarios = usuarioDAO.findAll(); // Traé nombre,email, telefono, idTipoUsuario
+	    request.setAttribute("usuarios", usuarios);
+	    request.getRequestDispatcher("/views/admin/usuarios.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
